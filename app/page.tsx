fd7e66df2e85a2586af509fd3249d1c0024fa942
Adem ilter <ademilter@gmail.com>
2024-02-13 18:52:10 +0300

    fix: home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <main className="">
-      <h1>Hello world</h1>
+      <h1>Merhaba</h1>
+
+      <p>
+        Akaryakıt fiyatlarına ait listeyi görmek için şehir adıyla sorgulayınız.
+      </p>
+      <p>
+        Örnek: <a href="/api?sehir=istanbul">/api?sehir=istanbul</a>
+      </p>
     </main>
   );
 }
