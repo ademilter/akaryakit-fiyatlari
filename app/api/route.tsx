2181bdc0c8552945eb8e3a08e5090419e7ad134e
Adem ilter <ademilter@gmail.com>
2024-02-13 18:34:35 +0300

    add: filter by query param

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,10 +1,20 @@
+import { type NextRequest } from 'next/server'
 import {parse} from 'muninn';
 import {priceSchema} from "@/utils/schemas";
+import {CITY_NAMES} from "@/utils/const";
 
 export const revalidate = 3600
 
-export async function GET(request: Request) {
-    const url = URLs.istanbul
+
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams
+    const city = searchParams.get('city') || ""
+
+    if (CITY_NAMES.indexOf(city) === -1) {
+        return new Response("Invalid city name", {status: 400})
+    }
+
+    const url = `https://www.petrolofisi.com.tr/akaryakit-fiyatlari/${city}-akaryakit-fiyatlari`
 
     // fetch the data
     const istanbul = await fetch(url)
@@ -13,10 +23,13 @@ export async function GET(request: Request) {
     // parse the data
     const result = parse(istanbulText, priceSchema);
 
+    if (!result.tables) {
+        return new Response("Failed to parse the data", {status: 500})
+    }
+
     // return the data
-    return Response.json(result, {status: 200})
+    return Response.json(result.tables, {status: 200})
 }
 
-const URLs = {
-    istanbul: "https://www.petrolofisi.com.tr/akaryakit-fiyatlari/istanbul-akaryakit-fiyatlari"
-}
+
+
