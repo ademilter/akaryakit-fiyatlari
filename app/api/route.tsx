81a323ff8bc38f82799ce71733badbb752d765fc
Adem ilter <ademilter@gmail.com>
2024-02-13 18:46:56 +0300

    prettier

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,35 +1,33 @@
-import { type NextRequest } from 'next/server'
-import {parse} from 'muninn';
-import {priceSchema} from "@/utils/schemas";
-import {CITY_NAMES} from "@/utils/const";
+import { type NextRequest } from "next/server";
+import { parse } from "muninn";
+import { priceSchema } from "@/utils/schemas";
+import { CITY_NAMES } from "@/utils/const";
 
-export const revalidate = 3600
-
+export const revalidate = 3600;
 
 export async function GET(request: NextRequest) {
-    const searchParams = request.nextUrl.searchParams
-    const city = searchParams.get('city') || ""
+  const searchParams = request.nextUrl.searchParams;
+  const sehir = searchParams.get("sehir") || "";
 
-    if (CITY_NAMES.indexOf(city) === -1) {
-        return new Response("Invalid city name", {status: 400})
-    }
+  if (CITY_NAMES.indexOf(sehir) === -1) {
+    return new Response("Geçersiz şehir adı => ?sehir=istanbul", {
+      status: 400,
+    });
+  }
 
-    const url = `https://www.petrolofisi.com.tr/akaryakit-fiyatlari/${city}-akaryakit-fiyatlari`
+  const url = `https://www.petrolofisi.com.tr/akaryakit-fiyatlari/${sehir}-akaryakit-fiyatlari`;
 
-    // fetch the data
-    const istanbul = await fetch(url)
-    const istanbulText = await istanbul.text()
+  // fetch the data
+  const istanbul = await fetch(url);
+  const istanbulText = await istanbul.text();
 
-    // parse the data
-    const result = parse(istanbulText, priceSchema);
+  // parse the data
+  const result = parse(istanbulText, priceSchema);
 
-    if (!result.tables) {
-        return new Response("Failed to parse the data", {status: 500})
-    }
+  if (!result.tables) {
+    return new Response("Failed to parse the data", { status: 500 });
+  }
 
-    // return the data
-    return Response.json(result.tables, {status: 200})
+  // return the data
+  return Response.json(result.tables, { status: 200 });
 }
-
-
-
--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,42 +1,40 @@
 const priceSchema = {
-    schema: {
-        tables: {
-            selector: 'table.table-prices tr.price-row | array',
-            schema: {
-                ilce: 'td:nth-child(1)',
-                vMax95: {
-                    selector: 'td:nth-child(2)',
-                    schema: {
-                        value: 'span.with-tax',
-                        valueWithoutTax: 'span.without-tax',
-                    }
-                },
-                vMaxDiesel: {
-                    selector: 'td:nth-child(3)',
-                    schema: {
-                        value: 'span.with-tax',
-                        valueWithoutTax: 'span.without-tax',
-                    }
-                },
-                vProDiesel: {
-                    selector: 'td:nth-child(4)',
-                    schema: {
-                        value: 'span.with-tax',
-                        valueWithoutTax: 'span.without-tax',
-                    }
-                },
-                poGaz: {
-                    selector: 'td:nth-child(5)',
-                    schema: {
-                        value: 'span.with-tax',
-                        valueWithoutTax: 'span.without-tax',
-                    }
-                }
-            }
-        }
-    }
-}
+  schema: {
+    tables: {
+      selector: "table.table-prices tr.price-row | array",
+      schema: {
+        ilce: "td:nth-child(1)",
+        vMax95: {
+          selector: "td:nth-child(2)",
+          schema: {
+            fiyat: "span.with-tax",
+            fiyatKDVli: "span.without-tax",
+          },
+        },
+        vMaxDiesel: {
+          selector: "td:nth-child(3)",
+          schema: {
+            fiyat: "span.with-tax",
+            fiyatKDVli: "span.without-tax",
+          },
+        },
+        vProDiesel: {
+          selector: "td:nth-child(4)",
+          schema: {
+            fiyat: "span.with-tax",
+            fiyatKDVli: "span.without-tax",
+          },
+        },
+        poGaz: {
+          selector: "td:nth-child(5)",
+          schema: {
+            fiyat: "span.with-tax",
+            fiyatKDVli: "span.without-tax",
+          },
+        },
+      },
+    },
+  },
+};
 
-export {
-    priceSchema
-}
+export { priceSchema };
