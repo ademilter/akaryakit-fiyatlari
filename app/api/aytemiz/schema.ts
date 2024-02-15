83e9ff2fa5846af4eb42c78fc41b4bbaf64765dd
alperbayram <alperbayrm@hotmail.com>
2024-02-15 23:58:06 +0300

    feat: add aytemiz

---
+++ app/api/aytemiz/schema.ts
@@ -0,0 +1,36 @@
+const schema = {
+  schema: {
+    lastUpdate: {
+      fill: new Date().toUTCString(),
+    },
+    data: {
+      selector: "#fuel-price-table tbody tr",
+      type: "array",
+      schema: {
+        il: "td:nth-child(1) div",
+        benzin: {
+          selector: "td:nth-child(2)",
+          transform: (val: any) => parseFloat(val.replace(",", ".")),
+        },
+        motorin_dizel: {
+          selector: "td:nth-child(3)",
+          transform: (val: any) => parseFloat(val.replace(",", ".")),
+        },
+        motorin_optimum_dizel: {
+          selector: "td:nth-child(4)",
+          transform: (val: any) => parseFloat(val.replace(",", ".")),
+        },
+        kalorifer_yakiti: {
+          selector: "td:nth-child(5)",
+          transform: (val: any) => parseFloat(val.replace(",", ".")),
+        },
+        fuel_oil: {
+          selector: "td:nth-child(6)",
+          transform: (val: any) => parseFloat(val.replace(",", ".")),
+        },
+      },
+    },
+  },
+};
+
+export { schema };
